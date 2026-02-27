9fcfbda16a95e324af2ab8cbe6fdbb0ce5eb82b3
DSA Developer <developer@example.com>
2026-02-27 23:36:57 +0530

    add move zeroes code

---
+++ Array/moveZeros.js
@@ -0,0 +1,16 @@
+var moveZeroes = function (nums) {
+  let index = 0;
+  for (let i=0; i<nums.length; i++){
+    if(nums[i] !== 0){
+        nums[index] = nums[i];
+        index++;
+    }
+
+  }
+  while(index < nums.length){
+    nums[index] = 0;
+    index++;
+  }
+    return nums;
+};
+console.log(moveZeroes([0,1,0,3,12]));
